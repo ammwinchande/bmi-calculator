e614e1bfe0bea5412207e7419a118445aefe345e
Muhammad A. Mwinchande <ammwinchande@gmail.com>
2020-07-30 17:08:58 +0300

    refactor card for all our layouts

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+// const: Colors to be used
+const bottomContainerHeight = 80.0;
+const bottomContainerColor = Color(0xFFEB1555);
+const activeCardColor = Color(0xFF1D1E33);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -12,9 +17,82 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: MainCard(
+                      cardColor: activeCardColor,
+                    ),
+                  ),
+                  Expanded(
+                    child: MainCard(
+                      cardColor: activeCardColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: MainCard(
+                      cardColor: activeCardColor,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: MainCard(
+                      cardColor: activeCardColor,
+                    ),
+                  ),
+                  Expanded(
+                    child: MainCard(
+                      cardColor: activeCardColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: bottomContainerColor,
+              margin: EdgeInsets.only(top: 15.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
+              // width: MediaQuery.of(context).size.width,
+              child: FlatButton(onPressed: () {}, child: Text('calculate BMI')),
+            )
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class MainCard extends StatelessWidget {
+  final Color cardColor;
+  final Widget cardChild;
+
+  MainCard({@required this.cardColor, this.cardChild});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: cardColor,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      margin: EdgeInsets.all(15.0),
+      child: cardChild,
     );
   }
 }
